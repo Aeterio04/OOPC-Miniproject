2c28d25f288e074911c79603f5f8965cb8a9702d
Aeterio04 <ojsangwai17@gmail.com>
2025-05-11 10:13:13 +0530

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <iomanip>
+
 
 using namespace std;
 
@@ -11,16 +13,19 @@ class ShopperStop{
     static vector<string> usernames;
     static vector<string> password;
     static vector<string> email;
+    static vector<int> superuserstat;
+
     public:
     // along with that it also acts as a abstract class, creating layouts for the auth class
     virtual int checksignupentry(string input_email, string input_username, string input_pass)=0;
     virtual int verifyloginentry(string input_email, string input_pass)=0;
 
 };
-vector<string> ShopperStop::usernames;
-vector<string> ShopperStop::password;
-vector<string> ShopperStop::email;
-
+vector<string> ShopperStop::usernames={"admin"};
+vector<string> ShopperStop::password={"SuperIsHere"};
+vector<string> ShopperStop::email={"admin1711@gmail.com"};
+vector<int> ShopperStop::superuserstat={1};
+    
 class Auth:public ShopperStop{
     // heavy lifting class
     public:
@@ -52,6 +57,8 @@ class Auth:public ShopperStop{
         usernames.push_back(input_username);
         password.push_back(input_pass);
         email.push_back(input_email);
+        superuserstat.push_back(0);
+        
         return 1;
         //as all are pushed together the user will have all the data stored at the same index at every vector, kindoff like a triple hashed map
 
@@ -82,12 +89,33 @@ class Auth:public ShopperStop{
     }
 };
 
+class superuser:public Auth{
+    //class specifically for superuser capabilities
+    //no data required
+    public:
+    void show_data(){
+        cout << left
+         << setw(6) << "UserId"
+         << setw(15) << "Username"
+         << setw(25) << "Email"
+         << right << setw(10) << "Admin" << endl;
+    // Data rows
+    for (int i = 0; i < usernames.size(); ++i) {
+        cout << left
+             << setw(6) << i
+             << setw(15) << usernames[i]
+             << setw(25) << email[i]
+             << right << setw(10) << showpoint << superuserstat[i]<< endl;  
+    }
+    }
+};
 class user{
-    
+    //the connection is made here as superuser is also a type of user
     private:
     string email;
     string pass;
     string username;
+    int superuserstatus=0;
 
     public:
 
@@ -110,13 +138,14 @@ class user{
     string put_pass(){
         return pass;
     }  
-     string put_username(){
+    string put_username(){
         return username;
     }
 
+
 };
 
-class customer: private user,public Auth{
+class customer: public user,public Auth{
     public:
     void signup(){
         string temp1,temp2;
@@ -137,7 +166,7 @@ class customer: private user,public Auth{
 
     }
 
-    void login(){
+    int login(){
         string temp1,temp2;
         cout<<"Enter Your email: ";
         cin>>temp1;
@@ -147,34 +176,100 @@ class customer: private user,public Auth{
         cin>>temp1;
         enterpass(temp1);
 
-        verifyloginentry(put_email(),put_pass());
+        return verifyloginentry(put_email(),put_pass());
     }
 
 };
 
-int main(){
-    customer c;
-      int choice;
-        while (choice!=3) {
-            cout << "====== MENU ======\n";
-            cout << "1. Login\n";
-            cout << "2. Signup\n";
-            cout << "3. Exit\n";
-            cout << "Enter your choice: ";
-            cin >> choice;
+class System: public customer,public superuser{
+    private:
+    int userid=-1;
 
-            switch (choice) {
-                case 1:
-                    c.login();
-                    break;
-                case 2:
-                    c.signup();
-                    break;
-                case 3:
-                    cout << "Exiting...\n";
-                    continue;
-                default:
-                    cout << "Invalid choice. Try again.\n";
+    public:
+    void run(){
+        int choice;
+        customer c;
+        while (choice!=3 ) {
+            if( userid==-1){
+                cout << "====== MENU ======\n";
+                cout << "1. Login\n";
+                cout << "2. Signup\n";
+                cout << "3. Exit\n";
+                cout << "Enter your choice: ";
+                cin >> choice;
+
+                switch (choice) {
+                    case 1:
+                        userid=c.login();
+                        break;
+                    case 2:
+                        c.signup();
+                        break;
+                    case 3:
+                        cout << "Exiting...\n";
+                        continue;
+                    default:
+                        cout << "Invalid choice. Try again.\n";
+                }
+            }
+            else{
+                if(superuserstat[userid]==0){
+                cout << "===== SHOP MENU =====\n";
+                cout << "1. View Items\n";
+                cout << "2. View Cart\n";
+                cout << "3. Previous Orders\n";
+                cout << "4. Current Balance\n";
+                cout << "5. Logout\n";
+                cout << "Enter your choice: ";
+                cin >> choice;
+
+                switch (choice) {
+                    case 1:
+                        cout<<"Showing Items\n";
+                        break;
+                    case 2:
+                        cout<<"Showing Cart\n";
+                        break;
+                    case 3:
+                        cout<<"Showing Previous Orders\n";
+                        break;
+                    case 4:
+                        cout<<"Showing current balance\n";
+                        break;
+                    case 5:
+                        cout << "Logging out...\n\n";
+                        userid=-1;
+                        break;
+                    default:
+                        cout << "Invalid choice. Try again.\n\n";
+                }}
+                else{
+                while (true) {
+                cout << "===== SUPERUSER PANEL =====\n";
+                cout << "1. View All Users\n";
+                cout << "2. Logout\n";
+                cout << "Enter your choice: ";
+                cin >> choice;
+
+                switch (choice) {
+                    case 1:
+                        show_data();
+                        break;
+                    case 2:
+                        cout << "Superuser logged out.\n\n";
+                        userid=-1;
+                        break;
+                    default:
+                        cout << "Invalid choice. Try again.\n\n";
+                }
+                }
             }
         }
     }
+    }
+};
+
+int main(){
+    System s;
+    s.run();
+}
